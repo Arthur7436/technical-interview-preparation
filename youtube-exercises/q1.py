8993e65e809fe57604b2b4c6ed9b3f023dbea646
arthur <arthur.thai479@gmail.com>
2022-09-26 22:16:04 +1000

    commit

--- youtube-exercises/q1.py
+++ youtube-exercises/q1.py
@@ -1,15 +1,23 @@
 # write a program which performs add, substract, mulitply and divide of two numbers
 
-# create variables and allow user to input the two numbers
+# allow user to input first number
+    
+    
 
-num1 = input('Enter the first number: ')
+num1 = input('Enter the first number: ')   
 try:
-    num1 = int(num1)
+    num1 = int()
 except ValueError:
     print('Invalid number, please try again')
     num1 = input('Enter the first number: ')
+    
+    
 num2 = input('Enter the second number: ')
-
+try:
+    num2 = int(num2)
+except ValueError:
+    print('Invalid number, please try again')
+    num2 = input('Enter the second number: ')
 # program calculations according to the users inputted numbers
 
 add = int(num1) + int(num2)
