3b00e833e3732bf2b15377c8a122305e0d7f14ea
Arthur <107759171+Arthur7436@users.noreply.github.com>
2022-09-26 10:42:55 +1000

    Delete FDM directory

--- FDM/practice-2.py
+++
@@ -1,4 +0,0 @@
-#write a program that enters users name returns a 'Hello' to the user
-
-name = input('Enter name: ')
-print('Welcome ',name)
--- FDM/practice-4.py
+++
@@ -1,9 +0,0 @@
-# Calculating the sum of a string of integers
-
-sum = 0
-print('Before ', sum)
-for number in (1, 58, 69, 4, 7, 6, 56):
-    sum = number + sum
-    print(sum, number)
-print('Sum of numbers: ', sum)
-
