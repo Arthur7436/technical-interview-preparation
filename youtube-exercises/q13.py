f3fb7c71f7e3b603a82cddb64388d2702da5be40
arthur <arthur.thai479@gmail.com>
2022-09-30 14:11:02 +1000

    difficult

--- youtube-exercises/q13.py
+++ youtube-exercises/q13.py
@@ -2,5 +2,32 @@
 x = [[2,7,3],[4,5,6],[7,8,9]]
 y = [[5,8,1],[6,7,3],[4,5,9]]
 
+result = [[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] + y[i][j]
+for r in result:
+    print('Addition:',r)
+    
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] - y[i][j]
+for r in result:
+    print('Subtraction:',r)
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] * y[i][j]
+for r in result:
+    print('Multiplication:', r)
+    
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] / y[i][j]
+for r in result:
+    print('Divison:',r)
+
+    
 
 
