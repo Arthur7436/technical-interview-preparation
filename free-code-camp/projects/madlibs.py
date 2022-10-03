07dbdb34c349440b954b6c8a01a2de2bab15ecaa
arthur <arthur.thai479@gmail.com>
2022-10-03 16:24:01 +1100

    refining spaces between words

---
+++ free-code-camp/projects/madlibs.py
@@ -0,0 +1,11 @@
+# Allow the user to input an adjective, verbs x 2 and a famous person
+# Output should be a madlib of a random sentence
+
+adjective = input('Adjective: ')
+verb_one = input('Verb: ')
+verb_two = input('Verb: ')
+famous_person = input('Famous person: ')
+
+print(
+    'Computer programming is so',adjective,'! It makes me so excited all the time because I love to',verb_one,'. Stay hydrated and',verb_two,' like you are',famous_person,'!'
+)
