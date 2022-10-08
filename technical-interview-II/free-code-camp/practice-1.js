50917c200bd8f7f24652f07e51402dfff81630e9
arthur <arthur.thai479@gmail.com>
2022-10-08 15:23:41 +1100

    free-code-camp/

--- technical-interview-II/free-code-camp/practice-1.js
+++ technical-interview-II/free-code-camp/practice-1.js
@@ -14,3 +14,6 @@ function nextInLine(arr, item) {
   console.log("Before: " + JSON.stringify(testArr));
   console.log(nextInLine(testArr, 6));
   console.log("After: " + JSON.stringify(testArr));
+
+  hello
+  
